231bf7add5c8a0e1609bd0784c363dbb96d5ed32
David Castellar Martínez <castellardav@gmail.com>
2023-03-06 23:45:14 -0500

    agrega test para funcion que obtine usuarios por id

--- tests/src/frameworks/repositories/userRepository.spec.ts
+++ tests/src/frameworks/repositories/userRepository.spec.ts
@@ -1,13 +1,16 @@
 import { Chance } from "chance";
+import { v4 as uuidv4 } from "uuid";
 import { User, constants } from "../../../../src/entities";
 import { userRepository } from "../../../../src/frameworks/repositories/inMemory";
 
 const chance = new Chance();
 
 describe("users repository tests", () => {
+  let mockData: any;
 
-  it("should add and return a new user with custom values", async () => {
-    const userData = new User({
+  beforeEach(() => {
+    mockData = {
+      id: uuidv4(),
       name: chance.name(),
       lastName: chance.last(),
       gender: constants.Genders.FEMALE,
@@ -16,20 +19,21 @@ describe("users repository tests", () => {
           color: "black",
         },
       },
-    });
+    }
+  })
+  it("should add and return a new user with custom values", async () => {
+    const mockUser = new User(mockData);
+    const newUser = await userRepository.add(mockUser);
 
-    const newUser = await userRepository.add(userData);
-
-    expect(newUser).toEqual(userData);
-    expect(newUser.name).toBe(userData.name);
-    expect(newUser.lastName).toBe(userData.lastName);
-    expect(newUser.gender).toBe(userData.gender);
-    expect(newUser.meta).toBe(userData.meta);
+    expect(newUser).toEqual(mockUser);
+    expect(newUser.name).toBe(mockUser.name);
+    expect(newUser.lastName).toBe(mockUser.lastName);
+    expect(newUser.gender).toBe(mockUser.gender);
+    expect(newUser.meta).toBe(mockUser.meta);
   });
 
   it("should add and return a new user with default values", async () => {
     const userData = new User({});
-
     const newUser = await userRepository.add(userData);
 
     expect(newUser).toEqual(userData);
@@ -39,9 +43,21 @@ describe("users repository tests", () => {
     expect(newUser.meta).toEqual({});
   });
 
-  it("should delete an existing user", async () => {});
+  it("should delete an existing user", async () => {
+
+  });
 
   it("should update an existing user", async () => {});
 
-  it("should get an existing user by id", async () => {});
+  it("should get an existing user by id", async () => {
+    const mockUser = new User(mockData);
+    await userRepository.add(mockUser);
+    const user = await userRepository.getById(mockData.id);
+
+    expect(user).toBeDefined();
+    expect(user?.name).toBe(mockData.name);
+    expect(user?.lastName).toBe(mockData.lastName);
+    expect(user?.gender).toBe(mockData.gender);
+    expect(user?.meta).toBe(mockData.meta);
+  });
 });
