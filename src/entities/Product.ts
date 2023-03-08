9e419be4f2268bc17874596d6b54f77b2701f52f
David Castellar Martínez <castellardav@gmail.com>
2023-03-08 00:42:44 -0500

    crea entidad para producto

---
+++ src/entities/Product.ts
@@ -0,0 +1,29 @@
+interface ProductType {
+  id?: string;
+  name?: string;
+  description?: string;
+  images?: any[];
+  price?: number;
+  color?: string;
+  meta?: object;
+}
+
+export class Product {
+  id: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  images: any[];
+  price: number | undefined;
+  color: string | undefined;
+  meta: object;
+
+  constructor(input: ProductType) {
+    this.id = input.id;
+    this.name = input.name;
+    this.description = input.description
+    this.images = input.images || [];
+    this.price = input.price;
+    this.color = input.color;
+    this.meta = input.meta || {};
+  }
+}
