3903c1e9d92960f1f5a87d51e8fd485c3896a5a8
David Castellar Martínez <castellardav@gmail.com>
2023-03-08 00:42:01 -0500

    cambia nulls por undefined

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -8,15 +8,15 @@ interface UserType {
 
 export class User {
   id: string | undefined;
-  lastName: string | null;
-  name: string | null;
+  lastName: string | undefined;
+  name: string | undefined;
   gender: Genders;
   meta: object;
 
   constructor(input: UserType) {
     this.id = input.id;
-    this.name = input.name || null;
-    this.lastName = input.lastName || null
+    this.name = input.name;
+    this.lastName = input.lastName
     this.gender = input.gender || Genders.NOT_SPECIFIED;
     this.meta = input.meta || {};
   }
