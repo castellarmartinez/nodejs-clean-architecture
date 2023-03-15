8770da892cf8b1af824fcfc46cbeb027a69101fe
David Castellar Martinez <castellardav@gmail.com>
2023-03-14 23:20:29 -0500

    create Order entity

---
+++ src/entities/Order.ts
@@ -0,0 +1,26 @@
+interface OrderType {
+   id?: string;
+   userId?: string;
+   productsId?: string[];
+   date?: Date;
+   isPayed?: boolean;
+   meta?: object;
+ }
+ 
+ export class Order {
+   id: string | undefined;
+   userId: string | undefined;
+   productsId: string[];
+   date: Date;
+   isPayed: boolean;
+   meta: object;
+ 
+   constructor(input: OrderType) {
+     this.id = input.id;
+     this.userId = input.userId;
+     this.productsId = input.productsId || [];
+     this.date = input.date || new Date();
+     this.isPayed = input.isPayed || false;
+     this.meta = input.meta || {};
+   }
+ }
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,8 +1,9 @@
 import { User, Genders } from "./User";
 import { Product } from "./Product";
+import { Order } from "./Order";
 
 const constants = {
   Genders,
 };
 
-export { User, constants, Product };
+export { User, constants, Product, Order};
