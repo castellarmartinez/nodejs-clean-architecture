75b3f39cc2adc61d5721956d96605b442ffde546
David Castellar Martínez <castellardav@gmail.com>
2023-04-07 10:36:49 -0500

    add update user use case

--- src/useCases/users/index.ts
+++ src/useCases/users/index.ts
@@ -1,4 +1,5 @@
 import { addUser } from "./addUserUseCase";
 import { getUserById } from "./getUserByIdUseCase";
+import { updateUser } from "./updateUserUseCase";
 
-export const userUseCase = { addUser, getUserById };
+export const userUseCase = { addUser, getUserById, updateUser };
