f9ac4ca479c7f36384c1d146cc298d7a55844eec
David Castellar Martínez <castellardav@gmail.com>
2023-04-13 23:08:39 -0500

    create delete user use case

---
+++ src/useCases/users/deleteUserUseCase.ts
@@ -0,0 +1,24 @@
+import createHttpError from "http-errors";
+
+import { User } from "../../entities";
+import { Constants } from "../../constants";
+import { UserType } from "../../entities/User";
+import { Dependencies } from "../../dependencies";
+
+export function deleteUser(dependencies: Dependencies) {
+  const { userRepository } = dependencies;
+
+  if (!userRepository) {
+    throw createHttpError(
+      Constants.httpErrors.USER_REPOSITORY_NOT_FOUD.httpCode,
+      JSON.stringify(Constants.httpErrors.USER_REPOSITORY_NOT_FOUD)
+    );
+  }
+
+  return (input: UserType = {}) => {
+    const { id, name, lastName, gender, meta } = input;
+    const user = new User({ id, name, lastName, gender, meta });
+
+    return userRepository.remove(user);
+  };
+}
--- src/useCases/users/index.ts
+++ src/useCases/users/index.ts
@@ -1,5 +1,6 @@
 import { addUser } from "./addUserUseCase";
 import { getUserById } from "./getUserByIdUseCase";
 import { updateUser } from "./updateUserUseCase";
+import { deleteUser } from "./deleteUserUseCase";
 
-export const userUseCase = { addUser, getUserById, updateUser };
+export const userUseCase = { addUser, getUserById, updateUser, deleteUser };
