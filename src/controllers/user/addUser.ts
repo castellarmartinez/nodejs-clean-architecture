6d68c83cf84cf2c6ae6241266cb330cc36fc6283
David Castellar Martínez <castellardav@gmail.com>
2023-05-29 22:03:15 -0500

    add addUser controller

--- src/controllers/user/addUser.ts
+++ src/controllers/user/addUser.ts
@@ -1,5 +1,22 @@
+import { NextFunction, Request, Response } from "express";
 import { Dependencies } from "../../dependencies";
+import { userUseCase } from "../../useCases/users";
+import { SuccessResponse } from "../../frameworks/common";
+import { UserType } from "../../entities/User";
 
-export default (dependencies: Dependencies) => {
-   const 
-}
+export default function(dependencies: Dependencies) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { body = {} } = req;
+      const { id, name, lastName, gender, meta }: UserType = body;
+
+      const addUser = userUseCase.addUser(dependencies);
+      const response = await addUser({id, name, lastName, gender, meta});
+      res.json(new SuccessResponse({status: true, content: response}));
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
