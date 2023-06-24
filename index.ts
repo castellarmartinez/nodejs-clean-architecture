881b5affc69bab96f8ddb7508a4d7d795ce38c06
David Castellar Martínez <castellardav@gmail.com>
2023-06-24 15:18:13 -0500

    refactor: update export to start the app

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import * as app from "./src/app";
+import app from "./src/app";
 
 app.start();
--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,15 @@ const app = express();
 const PORT = Number(process.env.PORT);
 const API_PREFIX = String(process.env.API_PREFIX);
 
-export function start() {
-  app.use(express.json());
-  app.use(express.urlencoded());
+export default {
+  start() {
+    app.use(express.json());
+    app.use(express.urlencoded());
 
-  app.use(API_PREFIX, routes(dependencies));
+    app.use(API_PREFIX, routes(dependencies));
 
-  app.listen(PORT, () => {
-    console.log(`Server up and running on port ${PORT}!`);
-  });
-}
+    app.listen(PORT, () => {
+      console.log(`Server up and running on port ${PORT}!`);
+    });
+  },
+};
