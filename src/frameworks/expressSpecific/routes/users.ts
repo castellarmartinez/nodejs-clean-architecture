bdabb0ff03f921896c76e4d36639ef83fb00ddeb
David Castellar Martínez <castellardav@gmail.com>
2023-06-24 16:07:56 -0500

    fix: user url routes

--- src/frameworks/expressSpecific/routes/users.ts
+++ src/frameworks/expressSpecific/routes/users.ts
@@ -6,8 +6,8 @@ export default (dependencies: any) => {
   const { addUser, getUserById, updateUser, deleteUser } =
     userControllers(dependencies);
 
-    router.route("./").post(addUser).delete(deleteUser).put(updateUser);
-    router.route("./:id").get(getUserById);
+    router.route("/").post(addUser).delete(deleteUser).put(updateUser);
+    router.route("/:id").get(getUserById);
 
     return router;
 };
