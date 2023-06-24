4452675555504e99f93e08d1a41749de32f1dd63
David Castellar Martínez <castellardav@gmail.com>
2023-06-24 15:13:19 -0500

    feat: add environment variables for loading the app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import "dotenv/config";
+
 import { routes } from "./frameworks/expressSpecific/routes";
 import dependencies from "./config/dependencies";
 
 const app = express();
-const localPort = 3000;
-const PORT = Number(process.env.PORT) || localPort;
+const PORT = Number(process.env.PORT);
 const API_PREFIX = String(process.env.API_PREFIX);
 
 export function start() {
