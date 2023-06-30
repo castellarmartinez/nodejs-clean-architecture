a2e38f209dfd268169f49357b4dc6d6b1a6d7e7b
David Castellar Martínez <castellardav@gmail.com>
2023-06-30 08:47:00 -0500

    refactor: add internal error path in error handler

--- src/frameworks/expressSpecific/errorHandler.ts
+++ src/frameworks/expressSpecific/errorHandler.ts
@@ -1,26 +1,35 @@
 import { SuccessResponse, ErrorResponse } from "../common";
 import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../common/response";
+import { Constants } from "../../constants";
 
 export default (err: unknown, req: Request, res: Response, _next: NextFunction) => {
+  let error: ErrorResponse;
+
   if (err instanceof HttpException) {
-    const error = new ErrorResponse({
-      status: err.status ?? 500,
-      msg: err.message ?? "No message",
-      reason: err.stack ?? "No reason",
+    error = new ErrorResponse({
+      status: err.status,
+      msg: err.message,
+      reason: err.stack ?? "No reason reported",
       url: req.originalUrl,
       ip: req.ip,
     });
-
-    res.status(error.status);
-    res.json(
-      new SuccessResponse({
-        status: false,
-        content: null,
-        error,
-      })
-    );
   } else {
-    res.json({Hola: "Mundo"});
+    error = new ErrorResponse({
+      status: Constants.httpErrors.INTERNAL_ERROR.httpCode,
+      msg: Constants.httpErrors.INTERNAL_ERROR.code,
+      reason: Constants.httpErrors.INTERNAL_ERROR.message,
+      url: req.originalUrl,
+      ip: req.ip,
+    });
   }
+
+  res.status(error.status);
+  res.json(
+    new SuccessResponse({
+      status: false,
+      content: null,
+      error,
+    })
+  );
 };
