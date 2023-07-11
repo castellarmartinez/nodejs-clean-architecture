1fb5352c08cf50afdcab7cfe8d7c16be7392e906
David Castellar Martínez <castellardav@gmail.com>
2023-07-11 16:53:10 -0500

    fix: delete express urlencoded middleware

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,6 @@ const API_PREFIX = String(process.env.API_PREFIX);
 export default {
   start() {
     app.use(express.json());
-    app.use(express.urlencoded());
 
     app.use(API_PREFIX, routes(dependencies));
     app.use(errorHandler);
