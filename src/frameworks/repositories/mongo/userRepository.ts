ab05317ae9d8f2262a37093968b165267bbaeffc
David Castellar Martínez <castellardav@gmail.com>
2023-07-14 17:58:37 -0500

    feat: add user repository for mongo

---
+++ src/frameworks/repositories/mongo/userRepository.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+import { userSchema } from "../../database/mongo/schemas";
+import { User } from "../../../entities";
+
+export class UserRepository {
+  entityName = "User";
+  User = mongoose.model(this.entityName, userSchema);
+
+  async add(user: User) {
+    const mongoObject = new this.User(user);
+    return mongoObject.save();
+  }
+
+  async update(updatedUser: User): Promise<User | null> {
+    const { id } = updatedUser;
+    delete updatedUser.id;
+
+    return this.User.findByIdAndUpdate(
+      id,
+      {
+        ...updatedUser,
+        updatedAt: new Date()
+      },
+      {
+        new: true,
+      }
+    ).lean();
+  }
+
+  async remove(id: string): Promise<true | null> {
+    return this.User.findByIdAndUpdate(
+      id,
+      {
+        deletedAt: new Date()
+      },
+      {
+        new: true,
+      }
+    ).lean();
+  }
+
+  async getById(id: string) {
+    return this.User.findOne({ _id: id });
+  }
+}
+
