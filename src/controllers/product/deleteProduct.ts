d604d43f19409f3023bbd4663244348c3af33cdb
David Castellar Martínez <castellardav@gmail.com>
2023-07-18 16:19:21 -0500

    fix: update controller for deleting products

--- src/controllers/product/deleteProduct.ts
+++ src/controllers/product/deleteProduct.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { Dependencies } from "../../dependencies";
-import { userUseCase } from "../../useCases/users";
+import { productUseCase } from "../../useCases/products";
 import { SuccessResponse } from "../../frameworks/common";
 
 export default function (dependencies: Dependencies) {
@@ -9,8 +9,8 @@ export default function (dependencies: Dependencies) {
       const { params = {} } = req;
       const { id } = params;
 
-      const deleteUser = userUseCase.deleteUser(dependencies);
-      const response = await deleteUser(id);
+      const deleteProduct = productUseCase.deleteProduct(dependencies);
+      const response = await deleteProduct(id);
       res.json(new SuccessResponse({ status: true, content: response }));
 
       next();
